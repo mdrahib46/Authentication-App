4831014908eafc0dadfa54609722b50755d64abd
mdrahib46 <nahid179846@bsdi-bd.org>
2025-12-31 23:23:41 +0600

    Feat: Delete account has been implemented with firebase auth

--- lib/pages/delete_account.dart
+++ lib/pages/delete_account.dart
@@ -1,9 +1,50 @@
+import 'package:authapp/appp/mobile/auth_service.dart';
 import 'package:authapp/constants/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class DeleteAccount extends StatelessWidget {
+class DeleteAccount extends StatefulWidget {
   const DeleteAccount({super.key});
+
+  @override
+  State<DeleteAccount> createState() => _DeleteAccountState();
+}
+
+class _DeleteAccountState extends State<DeleteAccount> {
+
+  final TextEditingController userEmail = TextEditingController();
+  final TextEditingController userPassword = TextEditingController();
+
+  String _errorMessage = '';
+
+  Future<void> deleteAccount() async{
+
+    try{
+      await authService.value.deleteAccount(userEmail: userEmail.text.trim(), userPassword: userPassword.text.trim());
+      if(mounted){
+        setState(() {
+          _errorMessage = '';
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Account has been deleted...!',
+                style: TextStyle(color: Colors.white),
+              ),
+              behavior: SnackBarBehavior.floating,
+              backgroundColor: Colors.teal.shade900,
+            ),
+          );
+        });
+        Navigator.pop(context);
+      }
+    } on FirebaseAuthException catch(e){
+      setState(() {
+        _errorMessage = e.message ?? "Something went wrong..!";
+      });
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +57,10 @@ class DeleteAccount extends StatelessWidget {
             children: [
               SizedBox(
                 width: 300,
-                child: Lottie.asset(
-                  'assets/lotties/TrashBin.json',
-                  repeat: false,
-                ),
+                child: Lottie.asset('assets/lotties/TrashBin.json', repeat: false),
               ),
               const SizedBox(height: 10),
-              Text('Delete Account', style: kTextStyle.pageTitle,),
+              Text('Delete Account', style: kTextStyle.pageTitle),
               TextFormField(decoration: InputDecoration(hintText: 'Email')),
               const SizedBox(height: 10),
               TextFormField(decoration: InputDecoration(hintText: 'Password')),
@@ -45,23 +83,12 @@ class DeleteAccount extends StatelessWidget {
                             ),
                           ],
                         ),
-                        content: Text(
-                          "Do you really want to delete your account ?",
-                        ),
+                        content: Text("Do you really want to delete your account ?"),
                         actions: [
-                          TextButton(onPressed: () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(
-                                  'Account has been deleted...!',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                behavior: SnackBarBehavior.floating,
-                                backgroundColor: Colors.teal.shade900,
-                              ),
-                            );
-                            Navigator.pop(context);
-                          }, child: Text("Delete")),
+                          TextButton(
+                            onPressed: deleteAccount,
+                            child: Text("Delete"),
+                          ),
                           TextButton(
                             onPressed: () {
                               Navigator.pop(context);
@@ -75,6 +102,7 @@ class DeleteAccount extends StatelessWidget {
                 },
                 child: Text("Delete"),
               ),
+              Text(_errorMessage, style: TextStyle(color: Colors.red),)
             ],
           ),
         ),
--- lib/pages/reset_password.dart
+++ lib/pages/reset_password.dart
@@ -1,14 +1,53 @@
+import 'package:authapp/appp/mobile/auth_service.dart';
 import 'package:authapp/constants/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-
-class ResetPassword extends StatelessWidget {
+class ResetPassword extends StatefulWidget {
   const ResetPassword({super.key});
 
   @override
+  State<ResetPassword> createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _oldPasswordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+
+  String _errorMessage = '';
+
+  Future<void> resetPassword() async {
+    try {
+      await authService.value.resetPasswordFormCurrentPass(
+        userEmail: _emailController.text.trim(),
+        oldPassword: _oldPasswordController.text,
+        currentPassword: _newPasswordController.text.trim(),
+      );
+
+      if (mounted) {
+        setState(() {
+          _errorMessage = '';
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Password has been updated...!', style: TextStyle(color: Colors.white)),
+              behavior: SnackBarBehavior.floating,
+              backgroundColor: Colors.teal.shade900,
+            ),
+          );
+        });
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = e.message ?? "Something went wrong";
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
         child: Padding(
@@ -17,37 +56,56 @@ class ResetPassword extends StatelessWidget {
             children: [
               SizedBox(
                 width: 300,
-                child: Lottie.asset(
-                  'assets/lotties/forgoPass.json',
-                  repeat: false,
-                ),
+                child: Lottie.asset('assets/lotties/forgoPass.json', repeat: false),
               ),
 
-              Text('Reset Password', style: kTextStyle.pageTitle,),
+              Text('Reset Password', style: kTextStyle.pageTitle),
               const SizedBox(height: 10),
-              TextFormField(decoration: InputDecoration(hintText: 'Email')),
-              const SizedBox(height: 10),
-              TextFormField(decoration: InputDecoration(hintText: 'Old Password')),
-              const SizedBox(height: 10),
-              TextFormField(decoration: InputDecoration(hintText: 'New Password')),
-              const SizedBox(height: 10),
-              FilledButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        'Password has been updated...!',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: Colors.teal.shade900,
-                    ),
-                  );
-
-
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _emailController,
+                validator: (value) {
+                  if (value!.isEmpty || value.trim().isEmpty) {
+                    return "Enter you email address";
+                  }
+                  final _emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                  if (!_emailRegex.hasMatch(value)) {
+                    return "Enter a valid email address";
+                  }
+                  return null;
                 },
-                child: Text("Reset Password"),
+                decoration: InputDecoration(hintText: 'Email'),
               ),
+              const SizedBox(height: 10),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _oldPasswordController,
+                validator: (value) {
+                  if (value!.isEmpty || value.trim().isEmpty) {
+                    return "Enter you old password";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(hintText: 'Old Password'),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _newPasswordController,
+                validator: (value) {
+                  if (value!.isEmpty || value.trim().isEmpty) {
+                    return "Enter you new password";
+                  }
+                  if (value.length < 6) {
+                    return "Password must be at least 6 character";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(hintText: 'New Password'),
+              ),
+              const SizedBox(height: 10),
+              FilledButton(onPressed: resetPassword, child: Text("Reset Password")),
+              Text(_errorMessage, style: TextStyle(color: Colors.red)),
             ],
           ),
         ),
